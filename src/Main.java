5d55462131e73905b4e6402d59b7b4c4aadc3d25
xinyaoli <li651854292@gmail.com>
2024-03-16 01:42:05 -0700

    Add annotation for input

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /***
+         * c is ♣️；s is ♠️；d is ♦️；h is ♥️
+         * 11 is J; 12 is Q; 13 is K; 14 is A
+         */
         String[] deck1 = new String[]{"c5","s11","s13","s2","s14","d7","d6","h5","h6","c9","d14","d12","c2"};
         String[] deck2 = new String[]{"s14","s11","s13","s12","s10","s9","h8","h9","h6","s4","s5","s3","s2"};
         String[] deck3 = new String[]{"s14","s11","s13","s12","s10","s7","h8","h9","h6","c7","d9","d12","c12"};
@@ -11,7 +15,7 @@ public class Main {
         String[] deck6 = new String[]{"s14","s10","s8","s3","s2","s6","h2","h9","h6","c9","d9","d12","c12"};
         String[] deck7 = new String[]{"s14","s10","s9","s3","s2","s6","h14","h9","h6","c9","d9","d14","c12"};
         String[] deck8 = new String[]{"s14","s10","s9","s3","s2","s6","h14","h11","h6","c10","d4","d5","c12"};
-        TopNCombination topNCombination = new TopNCombination(deck2);
+        TopNCombination topNCombination = new TopNCombination(deck8);
         topNCombination.printTopNCombination(10);
     }
 }
